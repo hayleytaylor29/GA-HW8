e82e0f482d4d6b3140744215077c51ba21629d48
hayleytaylor29 <hft929@gmail.com>
2021-04-11 20:51:56 -0400

    Magic 8 Ball express application created.

--- First_Express_HW/server.js
+++ First_Express_HW/server.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -11,12 +12,42 @@ app.listen(port, () => {
     console.log('app is running on port: ', port);
 })
 
-app.get('/tip/:total/:tipPercentage', (req,res) => {
-    //divide the tip percentage by 100 and store that in a variable called tipPercentage
-    let tipPercentage = Number(req.params.tipPercentage) / 100
-    //multiply the tipPercentage by the total to get the tip amount, store in a variable
-    //called tip
-    let tip = Number(req.params.total) * tipPercentage
-    //respond with the grand total, which is the total plus the tip
-    res.send("Your grand total is " + (tip + Number(req.params.total)))
+// app.get('/tip/:total/:tipPercentage', (req,res) => {
+//     //divide the tip percentage by 100 and store that in a variable called tipPercentage
+//     let tipPercentage = Number(req.params.tipPercentage) / 100
+//     //multiply the tipPercentage by the total to get the tip amount, store in a variable
+//     //called tip
+//     let tip = Number(req.params.total) * tipPercentage
+//     //respond with the grand total, which is the total plus the tip
+//     res.send("Your grand total is " + (tip + Number(req.params.total)))
+// })
+let responses = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes, definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'
+]
+app.get('/magic/:question', (req,res) => {
+    let answer = Math.floor(Math.random()*responses.length);
+    console.log(responses[answer]);
+    const ask = req.params.question;
+    const ans = responses[answer];
+    res.type('text/html');
+    res.send(ask + '<h1>' +ans+ '</h1>');
 })
